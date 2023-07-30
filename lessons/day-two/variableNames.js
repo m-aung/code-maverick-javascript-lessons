22f68024104d19ddef9eb7d21b11a0ddc85860b2
m-aung <myoa86g@gmail.com>
2023-07-30 17:26:24 -0400

    feat: add day two lesson

---
+++ lessons/day-two/variableNames.js
@@ -0,0 +1,22 @@
+// vairable names best practice [camelCase], eg python[snake case]
+//
+// var myName = 'Michael'
+// var myCountry = 'Myanmar'
+// var firstWeekOfTheDay = 'Monday'
+// var whatILoveIs = 'Money'
+// var firstPlace = 'David'
+// var location1 = 'Yangon'
+// snake case with capital letters
+//
+// var MY_NAME = 'Yuri'
+// var HOUSE_1_IS_IN = 'Hlaing'
+
+// invalid names
+// variables names cannot be start with numbers
+// cannot include special characters expect underscore '_'
+//
+// var 1stPlace = 'not valid'
+// var 12345 = 'not valid'
+// var :;- = 'not valid' including any special characters
+// var my name = 'not valid'
+
---
+++ lessons/day-two/variables.js
@@ -0,0 +1,71 @@
+// Variable
+// a container for a value with label/name
+// there are three keywords to store a variable
+/**
+ * 1. var
+ * 2. let
+ * 3. const
+ */
+// the stored values can be reused, emutable or unemutable for different purposes
+// we store values, like numbers that we might use it in Maths calculations, or a string that we might use as a part of a sentence
+
+// What is string?
+// A string can be a collection of a character or characters, a phrases of words or a full sentence
+// example
+// 'a', '1', '%' , ' '
+
+// var keyword
+//
+// var greeting = 'Hello World'
+// name : greeting
+// type : string
+//
+// console.log(greeting)
+
+// re-assign the variable to number
+//
+// greeting = 100
+// name : greeting
+// type : string
+//
+// console.log(greeting)
+
+// re-assign the variable to different string
+//
+// greeting = 'How are you?'
+// name : greeting
+// type : string
+//
+// console.log(greeting)
+
+// let keyword
+// almost the same as var
+//
+// let textBookName = 'Maths'
+// console.log(textBookName)
+
+// change textBookName to number
+//
+// textBookName = 250
+// console.log(textBookName)
+
+
+// const keyword
+//
+// a different kinds of keyword
+// example 1
+//
+// const myBottleColor = 'Red'
+// console.log(myBottleColor)
+
+// throws an runtime error
+// 
+// myBottleColor = 'Blue' // TypeError: Assignment to constant variable
+// example 2
+// 
+// const myLuckyNumber = 5
+// console.log(myLuckyNumber)
+
+// throws an runtime error
+// 
+// myLuckyNumber = 9 // TypeError: Assignment to constant variable
