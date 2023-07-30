134d9c2c18fb72f857fa5752868b0cbf4111426f
m-aung <myoa86g@gmail.com>
2023-07-29 20:04:33 -0400

    feat: add first lesson for day-one

---
+++ lessons/day-one/helloWorld.js
@@ -0,0 +1,23 @@
+// print Hello World
+//
+// console.log('Hello World')
+
+// string
+//
+// console.log(Hello World)
+
+// number
+// 
+// console.log(101)
+// console.log(10.5)
+
+
+
+
+// Bonus Lesson [Part-One]
+// how to write lines that the doesn't execute
+// for instance,
+// comments , notes, todos, or debugging
+
+
+
---
+++ lessons/day-one/variable-exercises.js
@@ -0,0 +1,13 @@
+// Exercises
+
+// print your first name and last name separated by a space
+
+// print the result of the sum of 5 and 2
+
+// try the following examples
+// 
+// console.log('1' + '2') // output: ?
+// console.log('1' + 2) // output: ?
+// console.log(2 - 1) // output: ?
+// console.log('2' - '1') // output: ?
+// console.log('2' - 1) // output: ?
