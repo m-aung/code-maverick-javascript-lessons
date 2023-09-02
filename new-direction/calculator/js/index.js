3dab4b7a9a40d253967f01285d405063d570b306
m-aung <myoa86g@gmail.com>
2023-09-01 22:55:28 -0400

    feat: initialize calculator page

---
+++ new-direction/calculator/js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  addAddition()
+  addSubtraction()
+  addMultiplication()
+  addDivision()
+  setupInitialStore()
+  clearLastCalculation()
+  clearAllCalculation()
+	console.log('DOM loaded')
+})
